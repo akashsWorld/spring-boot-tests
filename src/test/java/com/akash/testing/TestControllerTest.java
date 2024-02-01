9c872e3423a61da28457f2fcbcac5bea7dd17cb3
akashsWorld <akashbiswas.271999@gmail.com>
2024-02-01 14:11:54 +0530

    Test the content type of the data return from the controller.

--- src/test/java/com/akash/testing/TestControllerTest.java
+++ src/test/java/com/akash/testing/TestControllerTest.java
@@ -9,8 +9,11 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.BDDMockito.given;
 
 @WebMvcTest
 public class TestControllerTest {
@@ -21,11 +24,27 @@ public class TestControllerTest {
     @MockBean
     private TestService testService;
 
+    private final TestService testServiceObj = new TestService();
+
     @Test
     void getNameWithWish() throws Exception {
         mockMvc.perform(get("/api/v1/test/getNameWithWish/"+"Akash")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
+
+
+    @Test
+    void getNameWithWishAndAlsoCheckTheContentType() throws Exception {
+
+        String name = "Akash";
+        given(testService.getNameWithWish(name)).willReturn("Hello "+ name);
+
+        mockMvc.perform(get("/api/v1/test/getNameWithWish/"+name)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
 
 }
